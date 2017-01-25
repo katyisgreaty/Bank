468954cadd3740b069cd5d1d3151d9b0fb36472c
Katy Daviscourt and Shruti Priya <katy@>
2017-01-24 16:05:20 -0800

    write first attempt js for all specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
 // Business logic goes here.....
-var BankAccount =function(currentBalance,deposit,withdrawal){
- this.currentBalance = ;
+var BankAccount =function(balance,deposit,withdrawal){
+ this.currentBalance = 0;
  this.deposit = deposit;
  this.withdrawal = withdrawal;
-
-
 }
+ BankAccount.prototype.deposit = function(balance){
+   newBalance = currentBalance + deposit;
+   return newBalance;
 
+
+ }
+ BankAccount.prototype.withdraw = function(balance){
+   newBalance = currentBalance - withdrawal;
+   return newBalance;
+  if (newBalance <= 0){
+    alert ("You have Insufficient funds in your account.");
+  }
+ }
 
 
 
@@ -30,3 +40,5 @@ var BankAccount =function(currentBalance,deposit,withdrawal){
 $(document).ready(function(){
   $("form#BankAccount").submit(function(event){
     event.preventDefault();
+
+  var initialBalance = $("input#initialAmount").val();
